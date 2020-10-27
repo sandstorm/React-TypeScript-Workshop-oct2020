1e78d8411b6e6f5d219a83f505f8ff3707a06a5d
Robert Baruck <robert.baruck@sandstorm-media.de>
2020-10-27 17:48:17 +0100

    TASK: Add first own static Component

---
+++ src/Intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+    return (
+        <div className="intro">
+            <h1>React + TypeScript - Workshop</h1>
+            <p>Never Stop Learning!</p>
+        </div>
+    )
+}
+
+export default Intro
